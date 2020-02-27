6e5172d2beeac11af0370ebd8ae86408f25fae00
freefmly <52983002+freefmly@users.noreply.github.com>
2020-02-28 02:57:32 +0900

    go

---
+++ weather.js
@@ -0,0 +1,61 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "0a889ee909743e3f1dc45eaa8020c864";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const feelslike = json.main.feels_like;
+      const place = json.name;
+      const sky = json.weather[0].description;
+      weather.innerText = `위치: ${place}\n온도: ${temperature} ℃\n체감: ${feelslike}\n하늘: ${sky}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log(`위치 정보를 읽어올 수 없습니다.`);
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+  setInterval(loadCoords, 30000);
+}
+
+init();
